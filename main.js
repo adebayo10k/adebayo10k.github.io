d8c18335d7c69b51d00e6b2e7b0cd0758e51559a
damola adebayo <adebayo10k@gmail.com>
2020-05-03 08:51:00 +0100

    changes some variable and id names

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // here I fake my own expiry 
 const daily_ms = 1E+3*Math.pow(60,2)*24;
-let expiry_date = new Date(2020, 4, 6, 5, 22, 1, 972); // zero based month, UTC accounted
+let expiry_date = new Date(2020, 4, 5, 11, 22, 1, 972); // zero based month, UTC accounted
 let current_date = new Date();
 
 let days_left = Math.floor( (expiry_date.getTime() - current_date.getTime()) / daily_ms);
@@ -48,29 +48,27 @@ else{
     `;     
 }
 
-
-
-let code_text = document.getElementById("code_cw"); // codeBlockElemCw
-let show_button = document.querySelector(".show_button"); // use id show_btn_cw
+let codeBlockElemCw = document.getElementById("code_frag_block_cw"); // codeBlockElemCw, code_frag_block_cw
+let showBtnElemCw = document.getElementById("show_btn_cw"); // use id show_btn_cw
 
 // 
 let showCode = function (flag) {
     if (flag) {
-        code_text.innerHTML = codeText;
-        show_button.setAttribute("value", "hide code");
+        codeBlockElemCw.innerHTML = codeText;
+        showBtnElemCw.setAttribute("value", "hide code");
     }
     else {
-        code_text.innerHTML = "";
-        show_button.setAttribute("value", "show code");
+        codeBlockElemCw.innerHTML = "";
+        showBtnElemCw.setAttribute("value", "show code");
     }
 };
 
 showCode(false);
 
 // in functional, we'll have to pass the related button and codeblockelem objects in 
-// in oo, we'd just pass a cwSolution or hrSolution object, and access its' button and cbe properties as required
-show_button.addEventListener("click", function(){
-    if (code_text.innerHTML == ""){
+// in oo, we'd just pass ref to a cwSolution or hrSolution object  and access its' button and cbe properties as required
+showBtnElemCw.addEventListener("click", function(){
+    if (codeBlockElemCw.innerHTML == ""){
         showCode(true);
     }
     else{
@@ -78,9 +76,20 @@ show_button.addEventListener("click", function(){
     }
 });
 
-//-----------------------------------------------
-
-// 2020-05-02T09:11:39.299Z  
+//-----------------------------------------------------------------------------------------
+/*
+// define generic solution object
+let Solution = function(){}
+// define cWsolution object
+let CWsolution = function(){
+    __proto__ : Solution;
+}
+// define hRsolution object
+let HRsolution = function(){
+    __proto__ : Solution;    
+}
+*/
+// TODO:  
 // adds finally privacy comment text 
 // adds code to separate cw and hr functions
 // refactors everything to functions 
@@ -89,8 +98,8 @@ show_button.addEventListener("click", function(){
 
 /*
 FINALLY, SOME PRIVACY...
-This is where legend has it that tech companies put those job ads - for the eyes of the curious only. Unfortunately, it's just me this time....
-My focus is on becoming a better Software Developer and building great stuff that I genuinely care about.
+This is where legend has it that tech companies put those job ads - for the eyes of the curious only. Unfortunately, it's just me this time....and you.
+TODO: Become a better Software Developer and build great stuff that I genuinely care about.
 It seems that this SD game is all about the collaboration. Nobody has all the skills it takes to build great stuff.
-On that note, feel free to DM me on Slack if you're also into progressing, collaborating and social coding in general. Cheers.
+On that note, feel free to DM me on Slack if you're also into progressing, collaborating or social coding in general. Good Luck!.
 */
