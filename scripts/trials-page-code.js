fe2e292cc243d3241a7de61cc7299a8c7cd60b7b
damola adebayo <adebayo10k@gmail.com>
2020-06-05 19:00:59 +0100

    puts stored settings check on every page-specific script

--- scripts/trials-page-code.js
+++ scripts/trials-page-code.js
@@ -1,4 +1,4 @@
-//
+// consult storage for latest background settings
 let htmlElem = document.querySelector("html");
 if (localStorage.getItem("bgColour")){
     htmlElem.style.backgroundColor = `${localStorage.getItem("bgColour")}`;    
--- scripts/yoruba-code.js
+++ scripts/yoruba-code.js
@@ -1,3 +1,9 @@
+// consult storage for latest background settings
+let htmlElem = document.querySelector("html");
+if (localStorage.getItem("bgColour")){
+    htmlElem.style.backgroundColor = `${localStorage.getItem("bgColour")}`;    
+}
+
 //
 // assign value of iframe src attribute, and get the resource AFTER page content has loaded
 let yorubaVideoEmbed01 = document.getElementById("yorubaVideoEmbed01");
