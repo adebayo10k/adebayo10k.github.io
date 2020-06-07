863bd54e4759decdb2d5026c18bc6f9d830ffb48
damola adebayo <adebayo10k@gmail.com>
2020-06-07 18:29:14 +0100

    creates directory for 'page-specific' scripts

---
+++ scripts/page-specific/devices-code.js
@@ -0,0 +1,6 @@
+// consult storage for latest background settings
+let htmlElem = document.querySelector("html");
+if (localStorage.getItem("bgColour")){
+    htmlElem.style.backgroundColor = `${localStorage.getItem("bgColour")}`;    
+}
+
