e61d2edc7679bf0791774345bdd800822e54e79c
damola adebayo <adebayo10k@gmail.com>
2020-06-18 13:53:21 +0100

    completes initial basic html form structure

---
+++ scripts/page-specific/yoruba-forms-code.js
@@ -0,0 +1,5 @@
+// consult storage for latest background settings
+let htmlElem = document.querySelector("html");
+if (localStorage.getItem("bgColour")){
+    htmlElem.style.backgroundColor = `${localStorage.getItem("bgColour")}`;    
+}
