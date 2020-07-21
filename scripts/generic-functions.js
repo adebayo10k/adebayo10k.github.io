82d1125339e892bbfed5656de9f82c6fd77eeb42
damola adebayo <adebayo10k@gmail.com>
2020-07-21 13:41:21 +0100

    corrects wording in footer

--- scripts/generic-functions.js
+++ scripts/generic-functions.js
@@ -12,7 +12,7 @@ siteLogo.innerHTML = logoText;
 const footie = document.querySelector("footer");
 
 const footerText = `<p>
-NOTE: The content of this siteLogo is not serious. It is not rigorous research. Assume the authors&apos; policy to be: publish first, check accuracy later (if ever). I don&apos;t mean my code. That should always be solid.
+NOTE: The content of this website is not serious. It is not rigorous research. Assume the authors&apos; policy to be: publish first, check accuracy later (if ever). I&apos;m not talking about my code. That should always be really solid.
 </p>`;
 
 footie.innerHTML = footerText;
