e54b450e30b1595330498a0a9168f83647429bd5
damola adebayo <adebayo10k@gmail.com>
2020-07-29 03:33:56 +0100

    adds 'create account' form with inline validation

--- scripts/page-specific/yoruba-forms-code.js
+++ scripts/page-specific/yoruba-forms-code.js
@@ -4,130 +4,240 @@ if (localStorage.getItem("bgColour")){
     htmlElem.style.backgroundColor = `${localStorage.getItem("bgColour")}`;    
 }
 
-// NOTE: JUST BASICS FOR NOW UNTIL WE'RE BETTER ON CSS AND DOM JS!
-// if possible, organise form elements into data structures over which we can iterate
-// perhaps put error array elements into an html list?
+// Code now gives real-time, live, inline validation, as well as submit time validation.
+// Character ranges specified
+// Password confirm now first checks that password is valid, before doing it's own validations
 
-// get hooks into contact form elements
-const contact_form = document.getElementById("contact_form");
-//const contact_form_fieldsets = contact_form.children;
-const alias_name = document.getElementById("alias_name");
-const alias_name_errors = document.getElementById("alias_name_errors");
-console.log(alias_name.value);
+// Single error line still only displays the first encountered error, even when >1 exist
+// User still doesn't know field requirements and constraints in advance
 
-// get hooks into interface form
-const team_interface_form = document.getElementById("team_interface_form");
+const form = document.getElementById("form");
+// the input elements themselves...
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
-//console.log(contact_form);
-//console.log(contact_form_fieldsets );
-//console.log(team_interface_form);
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const validUsernameCharsetRegex = "/([\u0030-\u0039\u0041-\u005A\u0061-\u007A\u00C0-\u00DE\u00E0-\u00FF\u1E63\u1EB9\u1ECD])+/gu";
+
+const validLatinBasic = /^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/;
+const validLatinAll = /^[\u0041-\u005A\u0061-\u007A\u0030-\u0039\u0020\u00E0-\u00FF\u00C0-\u00DE\u1E63\u1EB9\u1ECD _-]+$/;
 /*
-for (i=0; i<contact_form_fieldsets.length; i++){
-    console.log(contact_form_fieldsets[i].children.item(1));
-}
+NUMERIC RANGE
+      \u0030-\u0039 numeric 0 - 9
+     BASIC LATIN RANGES
+      \u0041-\u005A basic latin upper-case A-Z
+      \u0061-\u007A basic latin lower-case a-z
+     LATIN-1 RANGES
+      \u00C0-\u00DE  latin upper-case A-Z
+      \u00E0-\u00FF  latin lower-case a-z
+     ENUMERATED DOTTED UNICODES (in numeric order)
+      \u1E63  s dot
+      \u1EB9  e dot
+      \u1ECD  o dot
+      
+      \u00E0-\u00FA à to ú
+      \u1EB9-\u1E63 ẹ to 
+      space is \u0020
 */
 
-const validNameCharsetRegex = "/([\u0030-\u0039\u0041-\u005A\u0061-\u007A\u00C0-\u00DE\u00E0-\u00FF\u1E63\u1EB9\u1ECD])+/gu";
+// =======================================================
+// ADD EVENT LISTENERS TO FORM CONTROLS
 
-let currentErrors = new Array();
+// form validation at submit time
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-const addErrorString = (errorString) => {
-    currentErrors.push(errorString);
-};
-
-// add eventListeners to form inputs
-alias_name.addEventListener("input", (event) => {
-    // each event is a completely new, independent test - so clear all at start
-    currentErrors = [];
-    alias_name_errors.innerHTML = "";
-    let liveErrorString = "";
-
-    if (checkForEmpty(alias_name.value)){
-        // pass a
-        console.log("alias name was empty");
-        addErrorString(`At least enter something.`);
-        //alias_name.setCustomValidity(`At least enter something.`); 
-        //alias_name.reportValidity();
-    }    
-    if (checkForTooShort(alias_name.value, alias_name.minLength)){
-        addErrorString(`Need at least ${alias_name.minLength - alias_name.value.length} more characters.`);
-    }
-
-    // check whether primary regex constrain is being met
-    if (!checkAgainstPrimaryRegex(alias_name.value, validNameCharsetRegex)){
-        addErrorString(`Oops, etwas ist vorboten.`);
-        for (char in alias_name.value){
-            console.log(alias_name.value[char]);
-            console.log(checkAgainstPrimaryRegex(alias_name.value[char], validNameCharsetRegex));
-        }
-    }
-
-    // reset TO EMPTY STRING: setCustomValidity("")
-
-    if (alias_name.validity.valid){
-        // if the field is valid, we remove any error messages
-        console.log("valid");
-        currentErrors = []; //moot
-        alias_name_errors.innerHTML = "";
-        alias_name_errors.className = "error nil"
-    }
-    else{
-        // add invalid class and remove valid class (or just reset className)
-        alias_name_errors.className = "error";
-
-        console.log(currentErrors);
-        for (errCount = 0; errCount < currentErrors.length; errCount++){
-            liveErrorString += `
-            ${currentErrors[errCount]}
-            `;
-            liveErrorString += `
-            `;
-        }
-        console.log(liveErrorString);
-        alias_name_errors.textContent = liveErrorString;
-        
-        
-    }
-
-    
-    
+  checkInputs();
 });
 
-    // in the submit event
-    //event.preventDefault();
+username.addEventListener("input", (event) => {
+  if (isEmpty(username)){
+    setErrorFor(username, "Username cannot be blank");
+  }
+  else if (isTooShort(username, username.minLength)){
+    setErrorFor(username, `Need at least ${username.minLength - username.value.length} more characters.`);
+  }
+  else if (hasInvalidCharacters(username)) {
+    setErrorFor(username, "Invalid character");
+  }
+  else {
+    setSuccessFor(username);
+  }  
+});
 
+email.addEventListener("input", (event) => {
+  if (isEmpty(email)){
+    setErrorFor(email, "Email cannot be blank");
+  }
+  else if (!isEmail(email)) {
+    setErrorFor(email, "Email is not valid");
+  }
+  else {
+    setSuccessFor(email);
+  }  
+});
+
+password.addEventListener("input", (event) => {
+  if (isEmpty(password)){
+    setErrorFor(password, "Password cannot be blank");
+  }
+  else if (isTooShort(password, password.minLength)){
+    setErrorFor(password, `Need at least ${password.minLength - password.value.length} more characters.`);
+  }
+  else {
+    setSuccessFor(password);
+  }  
+});
+
+password2.addEventListener("input", (event) => {
+  // before validating, just check that password is valid
+  if (password.parentElement.className === "form-control error") {
+    setErrorFor(password2, "Set a valid password first!");
+  }
+  else if (isEmpty(password2)){
+    setErrorFor(password2, "Password cannot be blank");
+  }
+  else if (password2.value !== password.value) {
+    setErrorFor(password2, `Passwords do not match`);
+  }
+  else {
+    setSuccessFor(password2);
+  }  
+});
+
+
+
+// =======================================================
+
+// show error, error class
+const setErrorFor = (input, message) => { 
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector("small");
+  let constraints = "Only letters, numbers, space, dash(-) and underscore(_) allowed";
+
+  // add error message into small
+  if (message == "Invalid character"){
+    small.innerHTML = `<abbr title="${constraints}">${message}</abbr>`;
+  }
+  else {
+    small.innerText = message;
+  }
+  
+  // add error class
+  formControl.className = "form-control error"
+};
+
+// add success class
+const setSuccessFor = (input) => {
+  const formControl = input.parentElement; //.form-control
+  formControl.className = "form-control success"
+};
+
+
+
+
+// =======================================================
+// CHECK ALL INPUTS AT SUBMIT TIME
+const checkInputs = () => {
+  // get values from all the inputs
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {    
+    setErrorFor(username, "Username cannot be blank");
+  }
+  else {    
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  }
+  else if (!isSubmitEmail(emailValue)) {
+    setErrorFor(email, "Email is not valid");
+  }
+  else {
+    // add success class
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === ""){
+    setErrorFor(password, "Password cannot be blank");
+  }
+  else {
+    setSuccessFor(password);
+  }
+
+  if (password2Value === "") {
+    setErrorFor(password2, "Password2 cannot be blank");
+  }
+  else if (password2Value !== passwordValue) {
+    setErrorFor(password2, "Passwords do not match");
+  }
+  else {
+    setSuccessFor(password2);
+  }
+};
+
+const isSubmitEmail = (input_value) => {
+  if (input_value.match(emailRegex)) {    
+    return true;
+  }
+  else {    
+    return false;
+  }
+};
+
+// =======================================================
+
+// =======================================================
+// SPECIFIC CHECKS ON INPUTS AT INPUT TIME
 
 // define a function for every validity check
 //===========================================
 
 // generalised function for checking...
-const checkForEmpty = (input_value) => {
-    let isEmpty = false;
-    if (input_value == ""){
-        isEmpty = true;
-    }
-    return isEmpty;
+const isEmpty = (input) => {
+  let input_value = input.value;
+  let isEmpty = false;
+  if (input_value == ""){
+      isEmpty = true;
+  }
+  return isEmpty;
 };
 
 // generalised function for checking...
-const checkForTooShort = (input_value, minLen) => {
-    isTooShort = false;
-    if (input_value.length < minLen){
-        isTooShort = true;
-    }
-    return isTooShort;
+const isTooShort = (input, minLen) => {
+  let input_value = input.value;
+  let isTooShort = false;
+  if (input_value.length < minLen){
+      isTooShort = true;
+  }
+  return isTooShort;
 };
 
-const checkForYorubaChar = (inputRef) => {
-    const yorubaCharsetRegex = "/([\u0030-\u0039\u0041-\u005A\u0061-\u007A\u00C0-\u00DE\u00E0-\u00FF\u1E63\u1EB9\u1ECD])+/gu";
+const isEmail = (input) => {
+  let input_value = input.value;
+  if (input_value.match(emailRegex)) {    
+    return true;
+  }
+  else {    
+    return false;
+  }
 };
 
-// generalised function for checking the content of any input against any regex
-const checkAgainstPrimaryRegex = (inputVal, primaryRegex) => {
-    let regexConstraintMet = false;
-    if (inputVal.match(primaryRegex)){
-        regexConstraintMet = true;
-        console.log("FOUND TRUE MATCH OK");
-    }
-    return regexConstraintMet;
+const hasInvalidCharacters = (input) => {
+  let input_value = input.value;
+  //if (!validUsernameCharsetRegex.test(input_value)) {
+  if (!input_value.match(validLatinAll)) {  
+    return true;
+  }
+  else {
+    return false;
+  }
 };
