24e1883997e27d912e15385915c075c30f597531
damola adebayo <adebayo10k@gmail.com>
2020-08-21 14:30:58 +0100

    removes commented out old code blocks

--- scripts/page-specific/yoruba-code.js
+++ scripts/page-specific/yoruba-code.js
@@ -26,59 +26,12 @@ yorubaAudioEmbed01.src = "https://w.soundcloud.com/player/?url=https%3A//api.sou
 
 const encodingsBody = document.getElementById("encodingsBody");
 
-/*
-
-//let encodingsRow = encodingsBody.childNodes;
-let encodingsRow = encodingsBody.children; // returns the rows of the table
-
-const yorubaMorse = {
-    "x0061" : ".-", //  a
-    "x00E0" : ".--.-", //    a grave   
-    "x00E1" : "",  //     a acute
-    "x0062" : "-...",  //     b
-    "x0064" : "-..",  //     d
-    "x0065" : ".",  //     e
-    "x00E8" : "",  //     e grave
-    "x00E9" : "..-..",  //     e acute
-    "x1EB9" : "",  //     e dot
-    "xNoHexeDotgrave" : "",  //     e dot grave
-    "xNoHexeDotacute" : "",  //     e dot acute
-    "x0066" : "..-.",  //     f
-    "x0067" : "--.",  //     g
-    "xNoHexgb" : "",  //     gb
-    "x0068" : "....",  //     h
-    "x0069" : "..",  //     i
-    "x00EC" : "",  //     i grave
-    "x00ED" : "",  //     i acute
-    "x006A" : ".---",  //     j
-    "x006B" : "-.-",  //     k
-    "x006C" : ".-..",  //     l
-    "x006D" : "--",  //     m
-    "x006E" : "-.",  //     n            
-    "x006F" : "---",  //     o          
-    "x00F2" : "",  //     o grave        
-    "x00F3" : "---.",  //     o acute        
-    "x1ECD" : "",  //     o dot          
-    "xNoHexoDotGrave" : "",  //     o dot grave        
-    "xNoHexoDotAcute" : "",  //     o dot acute        
-    "x0070" : ".--.",  //     p          
-    "x0072" : ".-.",  //     r        
-    "x0073" : "...",  //     s        
-    "x1E63" : "",  //     s dot          
-    "x0074" : "-",  //     t        
-    "x0075" : "..-",  //     u        
-    "x00F9" : "",  //     u grave          
-    "x00FA" : "",  //     u acute        
-    "x0077" : ".--",  //     w        
-    "x0079" : "-.--"  //     y
-};
-
-*/
-
 const morse = {
     "dot" : "&#x2022;",
     "dash" : "&#x2501;"
 };
+
+const yDataURL = "./data/yoruba-char-encodings.json";// from the root apparently
 
 // called functions ---------------------------------------------------------------
 
@@ -97,54 +50,6 @@ const createHexStringMorse = (punctString) => {
 
     return hexStringMorse;
 };
-
-/*
-const getMorseField = (key) => {
-    let targetString = `&#${key};`;
-    let targetMorseField = null;
-    //console.log(`targetString : ${targetString}`);
-    for (let i = 0; i < encodingsRow.length; i++) {
-        //console.log(`textcontent : ${encodingsRow[i].children.item(1).textContent}`);
-        if (encodingsRow[i].children.item(1).textContent == targetString){
-            targetMorseField = encodingsRow[i].children.item(4);
-            break;
-        }   
-    }
-    return targetMorseField;
-};
-
-
-// main ---------------------------------------------------------------
-
-// populate morse data column
-for (let key in yorubaMorse){    
-    if (yorubaMorse[key] != ""){
-        //console.log(key);
-        //console.log(yorubaMorse[key]);
-        let hexStringMorse = createHexStringMorse(yorubaMorse[key]);
-        //console.log(hexStringMorse);
-        
-        let targetMorseField = getMorseField(key);
-        //console.log(targetMorseField);
-        //console.log();
-        
-        // just make absolutely sure that a field was matched
-        if (targetMorseField != null){            
-            targetMorseField.innerHTML = `<p>${hexStringMorse}</p>`;
-        }
-    }
-}
-
-*/
-
-//----------------------------------------------------------------
-
-//const encodingsBody1 = document.getElementById("encodingsBody1");
-//let encodingsRow = encodingsBody.childNodes;
-//let encodingsRow1 = encodingsBody1.children; // returns the rows of the table
-
-
-const yDataURL = "./data/yoruba-char-encodings.json";// from the root apparently
 
 const getYorubaCharTableData = (url, responseType) => {
   return fetch(url).then(response => {
@@ -202,10 +107,6 @@ const populateTableBody = (jsonObj) => {
     encodingsBody.appendChild(row);
 
   }// end outer for loop
-
-
-
-
 };
 
 //------------- main ------------
